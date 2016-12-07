17e225456e133548600f23a55a4afcd68e60d651
Pawel Jankowski <jjpikoov@gmail.com>
2016-12-07 13:43:40 +0100

    postgres fix

--- sznk/repository/repository.py
+++ sznk/repository/repository.py
@@ -4,5 +4,5 @@ from sqlalchemy.orm.session import sessionmaker
 
 class Repository:
     Session = sessionmaker()
-    engine = create_engine('postgresql://admin:admin@localhost:5433/sznk_db')
+    engine = create_engine('postgresql://admin:admin@localhost:5432/sznk_db')
     Session.configure(bind=engine)
