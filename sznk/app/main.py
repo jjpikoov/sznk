3dc7c932a2c13ab5759a442fd0bce427dfa565a7
Pawel Jankowski <jjpikoov@gmail.com>
2016-12-07 12:50:41 +0100

    static resources now accessible

--- sznk/app/main.py
+++ sznk/app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import send_from_directory
 from flask.templating import render_template
 
 app = Flask(__name__)
@@ -6,6 +7,10 @@ app = Flask(__name__)
 @app.route("/")
 def show_main_page():
     return render_template('main.j2')
+
+@app.route('/resources/<path:path>')
+def access_static_resources(path):
+    return send_from_directory('resources', path)
 
 if __name__ == "__main__":
     app.run(debug=True)
